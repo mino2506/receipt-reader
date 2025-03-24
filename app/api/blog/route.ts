527e02a1167a0f8354696b0e5d5290d90f4b8377
liones_2612 <liones2612@gmail.com>
2025-03-24 23:09:29 +0900

    Create CURD API

---
+++ app/api/blog/route.ts
@@ -0,0 +1,58 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function main() {
+	try {
+		await prisma.$connect();
+	} catch (error) {
+		return Error("DB接続に失敗しました");
+	}
+}
+
+/**
+ * API: GET ALL POSTS
+ * supabaseからすべての記事を取得する。
+ * @param req - Request
+ * @param res - NextResponse
+ * @returns -Promise< NextResponse< { message; posts[]; } > | NextResponse< { message; error; } > >
+ */
+export const GET = async (req: Request, res: NextResponse) => {
+	console.log("\n\n🫴🎁~~~   GEEEEEEEEEET!!!💣💣💣💥💥💥\n");
+	try {
+		await main();
+		const posts = await prisma.post.findMany();
+		return NextResponse.json({ message: "Success", posts }, { status: 200 });
+	} catch (error) {
+		return NextResponse.json({ message: "Error", error }, { status: 500 });
+	} finally {
+		await prisma.$disconnect();
+	}
+};
+
+/**
+ * API: CREATE POST
+ * supabaseに新しい記事を作成する。
+ * @param req - Request
+ * @param res - NextResponse
+ * @returns - Promise< NextResponse< { message; post; } > | NextResponse< { message; error; } > >
+ */
+
+export const POST = async (req: Request, res: NextResponse) => {
+	console.log("\n\n~~~📨📮   POOOOOOOOOOST!!!💣💣💣💥💥💥\n");
+	try {
+		const { title, description } = await req.json();
+
+		await main();
+		const post = await prisma.post.create({
+			data: {
+				title,
+				description,
+			},
+		});
+		return NextResponse.json({ message: "Success", post }, { status: 201 });
+	} catch (error) {
+		return NextResponse.json({ message: "Error", error }, { status: 500 });
+	} finally {
+		await prisma.$disconnect();
+	}
+};
