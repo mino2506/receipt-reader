607e841c935ba183ec138c6e2c00be15a99ed45a
liones_2612 <liones2612@gmail.com>
2025-03-26 23:46:35 +0900

    OCR API by Google Cloud Vision

---
+++ lib/googleCloudVision.ts
@@ -0,0 +1,5 @@
+import vision from "@google-cloud/vision";
+
+export const googleCloudVisionClient = new vision.ImageAnnotatorClient({
+	keyFilename: process.env.GOOGLE_CLOUD_VISION_API_KEY ?? "",
+});
