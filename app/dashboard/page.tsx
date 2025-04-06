bf3319556c17de0e2fe192975f073c478926d34c
liones_2612 <liones2612@gmail.com>
2025-04-07 02:17:22 +0900

    OCR API のミドルウェア除外 receiptのpege作成開始

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@
 "use client";
 
 import SignOutButton from "@/app/components/SignOutButton";
+import ReceiptImage from "@/app/ocr/receipt/receiptImage/page";
 import { useSession } from "@/context/useSession";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -11,20 +12,23 @@ export default function Dashboard() {
 	const router = useRouter();
 	const { session, loading } = useSession();
 
-	useEffect(() => {
-		if (!loading && !session) {
-			router.push("/login");
-		}
-	});
+	// useEffect(() => {
+	// 	if (!loading && !session) {
+	// 		router.push("/login");
+	// 	}
+	// });
 
 	if (loading) return <div>読み込み中...</div>;
 
 	if (session) {
 		return (
-			<>
+			<div className="w-screen h-screen flex flex-col items-center justify-center p-3 bg-gray-100">
 				<SignOutButton />
-				<div>ようこそ、{session.user.email} さん！</div>
-			</>
+				<div className="flex-wrap m-3 bg-gray-500 max-w-full break-words">
+					ようこそ、{session.user.email} さん！aaaaaaaaaaaaaaaaaaa
+				</div>
+				<ReceiptImage />
+			</div>
 		);
 	}
 }
