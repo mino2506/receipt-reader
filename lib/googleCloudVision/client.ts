13cecd2c74d493fcf264778f48e01d806bf4acf3
liones_2612 <liones2612@gmail.com>
2025-10-12 23:42:07 +0900

    Change Gcv credentials setting

--- lib/googleCloudVision/client.ts
+++ lib/googleCloudVision/client.ts
@@ -1,7 +1,9 @@
 import vision from "@google-cloud/vision";
 
 export const googleCloudVisionClient = new vision.ImageAnnotatorClient({
-	projectId: process.env.GOOGLE_CLOUD_VISION_PROJECT_ID ?? "",
-	client_email: process.env.GOOGLE_CLOUD_VISION_CLIENT_EMAIL ?? "",
-	private_key: process.env.GOOGLE_CLOUD_VISION_API_KEY ?? "",
+	credentials: {
+		projectId: process.env.GOOGLE_CLOUD_VISION_PROJECT_ID ?? "",
+		client_email: process.env.GOOGLE_CLOUD_VISION_CLIENT_EMAIL ?? "",
+		private_key: process.env.GOOGLE_CLOUD_VISION_API_KEY ?? "",
+	},
 });
